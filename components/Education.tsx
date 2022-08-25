6f7f70df50703e316b91580f368f41174ac0025b
Lynne <lydhr@sina.com>
2022-08-25 15:01:47 -0400

    extract education into data json

--- components/Education.tsx
+++ components/Education.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import ExtLink from './ExtLink'
+import data from './data/personalInfo.json'
 
 const Education = (): JSX.Element => {
     return (
         <section className="grid" id="education">
             <h2 className="text-xl font-bold mt-12 mb-4">Education</h2>
             <p className = "text-base ml-4 text-gray-700">
-                🐸 &nbsp;was at <ExtLink href="https://harrypotter.fandom.com/wiki/British_Auror_Office"> The Auror Department(2007-2008, Internship)</ExtLink> 
-                &nbsp;and <ExtLink href="https://harrypotter.fandom.com/wiki/Hogwarts_School_of_Witchcraft_and_Wizardry">Hogwarts Primary University(1911-1988, Bachelor's in Magic)</ExtLink>
+                🐸 &nbsp;was at &nbsp;
+                {
+                    data.education.map((education, index) => {
+                        if (index == 0){
+                            return <ExtLink href="{education.link}">{education.name}</ExtLink>
+                        }else{
+                            return <ExtLink href="{education.link}">, {education.name}</ExtLink>
+                        }
+                    })
+                }
             </p>
         </section>
     );
