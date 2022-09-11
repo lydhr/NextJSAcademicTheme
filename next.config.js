cf907caec84f1e2e0a493cef8ec4011d29a0b915
Lynne <lydhr@sina.com>
2022-09-11 00:08:50 -0400

    upgrade next 11-12
    next-offline work-server customization become invalid properties (warnings in build log); audit fix

--- next.config.js
+++ next.config.js
@@ -1,36 +1,8 @@
 const withOffline = require('next-offline');
 
 const nextConfig = {
-	workboxOpts: {
-		swDest: 'static/service-worker.js',
-		runtimeCaching: [
-			{
-				urlPattern: /^https?.*/,
-				handler: 'NetworkFirst',
-				options: {
-					cacheName: 'https-calls',
-					networkTimeoutSeconds: 15,
-					expiration: {
-						maxEntries: 150,
-						maxAgeSeconds: 30 * 24 * 60 * 60 // 1 month
-					},
-					cacheableResponse: {
-						statuses: [0, 200]
-					}
-				}
-			}
-		]
-	},
-	reactStrictMode: true,
-	experimental: {
-		optimizeFonts: true,
-		optimizeImages: true,
-		optimizeCss: true
-	},
-    future: {
-        strictPostcssConfiguration: true
-	},
-	poweredByHeader: false
+	poweredByHeader: false,
+	swcMinify: true,
 };
 
 module.exports = withOffline(nextConfig);
