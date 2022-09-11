1262577c8bc9e106d763603073b3ed8c42e1cc90
Lynne <lydhr@sina.com>
2022-09-11 00:12:45 -0400

    add GoogleTag
    config in .env

---
+++ components/GoogleTag.tsx
@@ -0,0 +1,21 @@
+import Script from 'next/script';
+
+const GoogleTag = (): JSX.Element => {
+  return (
+    <>
+      <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+      <Script strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+          page_path: window.location.pathname,
+          });
+        `}
+      </Script>
+    </>
+  );
+};
+
+export default GoogleTag;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,8 @@ import Head from 'next/head';
 import {ThemeProvider, useTheme} from 'next-themes';
 
 import Layout from '../components/Layout';
-import personalInfo from '../components/data/personalInfo.json'
+import personalInfo from '../components/data/personalInfo.json';
+import GoogleTag from '../components/GoogleTag';
 
 import 'tailwindcss/tailwind.css';
 
@@ -13,11 +14,12 @@ const App = ({Component, pageProps}: Readonly<AppProps>): JSX.Element => {
 	return (
 		<ThemeProvider attribute="class">
 			<Head>
+				<GoogleTag/>
 				<title>{personalInfo.title}</title>
-				<link rel="canonical" href={personalInfo.domain} />
+				<link rel="canonical" href={personalInfo.domain}/>
 				<meta name="description" content={personalInfo.name+"'s personal website"}/>
 				<meta property="og:description" content={personalInfo.name+"'s personal website"}/>
-		        <meta property="og:image" content="/favicon/favicon.ico"/>
+				<meta property="og:image" content="/favicon/favicon.ico"/>
 				<meta property="og:locale" content="en_US"/>
 				<meta property="og:type" content="website"/>
 			</Head>
