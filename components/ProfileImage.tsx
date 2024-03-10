03d1489dd4ca0fa9ff4d2a7883145ee89afd1cd2
Lynne <lydhr@sina.com>
2024-03-10 18:50:58 -0400

    fix round image

--- components/ProfileImage.tsx
+++ components/ProfileImage.tsx
@@ -4,16 +4,14 @@ const ProfileImage = (): JSX.Element => {
     
     return (
         <Image
-            className="rounded-full select-none transition-all pointer-events-none rounded-md"
+            className="rounded-full select-none transition-all pointer-events-none"
             src="/images/me.jpg"
             // onMouseOver = {e => e.currentTarget.src = "/images/gelo.HEIC"}
-            
             // onMouseOut = {e => e.currentTarget.src = "/images/me.jpg"}
             draggable={false}
-            alt="My face"
+            alt="My profile image"
             width={260}
-            height={260}
-        />
+            height={260}/>
     );
 };
 
